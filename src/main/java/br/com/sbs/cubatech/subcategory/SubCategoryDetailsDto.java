9faf55eb8f8167eb3d7dec1f2a4f022c62aa8691
sergiobsilva2505 <sergio.bezerra@outlook.com>
2021-09-03 11:06:27 -0300

    ajustes finais

--- src/main/java/br/com/sbs/cubatech/subcategory/SubCategoryDetailsDto.java
+++ src/main/java/br/com/sbs/cubatech/subcategory/SubCategoryDetailsDto.java
@@ -7,13 +7,18 @@ import lombok.Getter;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Getter
 public class SubCategoryDetailsDto {
 
     private final String name;
     private final String urlCode;
     private final List<CourseDetailsDto> coursesDetails;
+
+    public SubCategoryDetailsDto(SubCategory subCategory) {
+        this.name = subCategory.getName();
+        this.urlCode = subCategory.getUrlCode();
+        this.coursesDetails = subCategory.getCourses().stream().map(CourseDetailsDto::new).toList();
+    }
 
 
 }
